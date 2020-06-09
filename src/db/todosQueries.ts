072beaebddae84e3b951404288aa2d305d86d99d
Chen Roth <rothchen@gmail.com>
2020-06-10 01:45:20 +0300

    add POST /todos

--- src/db/todosQueries.ts
+++ src/db/todosQueries.ts
@@ -4,3 +4,8 @@ export async function getTodos(userId: number) {
     const [todos] = await sql.execute('SELECT * FROM todos WHERE userId = ?', [userId]);
     return todos;
 }
+
+export async function createTodo(userId: number, description: string, dueDate: Date): Promise<number> {
+    const [{insertId: todoId}] = await sql.execute('INSERT INTO todos (userId, description, dueDate) VALUES (?, ?, ?)', [userId, description, dueDate]);
+    return todoId;
+}
--- src/routers/todosRouter.ts
+++ src/routers/todosRouter.ts
@@ -1,5 +1,7 @@
 import {Router} from 'express';
-import { getTodos } from '../db/todosQueries';
+import { getTodos, createTodo } from '../db/todosQueries';
+import { validateSchema } from '../middleware/validateSchema';
+import { todoSchema } from '../schemas/todoSchema';
 
 export const todosRouter = Router();
 
@@ -7,4 +9,11 @@ todosRouter.get('/', async (req, res) => {
     const { userId } = (req as any).user;
     const todos = await getTodos(userId);
     res.send(todos);
-})
+});
+
+todosRouter.post('/', validateSchema(todoSchema), async (req, res) => {
+    const {userId} = (req as any).user;
+    const {description, dueDate} = req.body;
+    const todoId = await createTodo(userId, description, dueDate);
+    res.send({todoId});
+});
